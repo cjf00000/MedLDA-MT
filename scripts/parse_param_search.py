1f6884bfbaaa33dfeda568b78dd0dc332993b987
Jianfei Chen <chrisjianfeichen@gmail.com>
2019-03-07 10:13:17 +0800

    Updated scripts

--- scripts/parse_param_search.py
+++ scripts/parse_param_search.py
@@ -1,4 +1,4 @@
-from utils import read_log
+from utils import *
 
 if __name__ == '__main__':
     tasks = []
@@ -19,6 +19,8 @@ if __name__ == '__main__':
     print(best_params, best_acc)
 
     # rcv1
+    best_acc = 0
+    best_params = []
     for K in [100, 200, 400, 600, 800, 1000]:
         for C in [0.1, 0.3, 1, 3, 10]:
             for ell in [0.1, 0.3, 1, 3, 10]:
@@ -29,5 +31,21 @@ if __name__ == '__main__':
                 if acc > best_acc:
                     best_acc = acc
                     best_params = (K, C, ell)
+    print(best_params, best_acc)
 
+    best_acc = 0
+    best_params = []
+    for K in [20, 40, 60, 80, 100]:
+        for C in [0.01, 0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 1.28]:
+            for ell in [1, 2, 4, 8, 16, 32, 64, 128]:
+                id = '20news_light_K{}_C{}_ell{}'.format(K, C, ell)
+                acc = read_light_log('../logs/param_search/{}.log'.format(id))
+                if len(acc) > 0:
+                    acc = acc[-1]
+                else:
+                    acc = 0
+                print(K, C, ell, acc)
+                if acc > best_acc:
+                    best_acc = acc
+                    best_params = (K, C, ell)
     print(best_params, best_acc)
--- scripts/utils.py
+++ scripts/utils.py
@@ -8,3 +8,11 @@ def read_log(file):
                 line = line.replace(", }", "}")
                 lines.append(json.loads(line))
     return lines
+
+def read_light_log(file):
+    with open(file) as f:
+        lines = []
+        for line in f:
+            if line.find('Test Accuracy') != -1:
+                lines.append(float(line.split()[-2]))
+    return lines
